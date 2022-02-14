858e20525ec5762f0113f3de6a9ff7602915a15e
Aman <amankeshari268@gmail.com>
2022-02-14 22:07:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    let d = new Date();
+    let hTime = d.getHours();
+    let mTime = d.getMinutes();
+    let sTime = d.getSeconds();
+    let hRotation = 30*hTime+mTime/2+sTime/120;
+    let mRotation = 6*mTime+sTime/10;
+    let sRotation = 6*sTime;
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
+},1000);
